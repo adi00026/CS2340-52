0374a13edde7a4604cbb03fe525b9a81b3c4ff8a
AniruddhaDas1998 <aniruddhadas1998@gmail.com>
2018-02-08 01:42:28 -0500

    Added Encryption class and util package

---
+++ app/src/main/java/edu/gatech/cs2340/vaspa/buzzshelter/util/Encryption.java
@@ -0,0 +1,38 @@
+package edu.gatech.cs2340.vaspa.buzzshelter.util;
+
+/**
+ * Created by aniruddhadas on 08/02/18.
+ */
+
+public class Encryption {
+
+    private static byte encryptionKey = 0x7A;
+
+    /**
+     * Takes in a string, encodes it and returns encoded string
+     *
+     * @param password String to be encoded
+     * @return encoded string
+     */
+    public static String encode(String password) {
+        char[] encodedString = new char[password.length()];
+        for (int i = 0; i < password.length(); i++) {
+            encodedString[i] = (char) ((byte)password.charAt(i) ^ encryptionKey);
+        }
+        return new String(encodedString);
+    }
+
+    /**
+     * Takes in a string, decodes it and returns decoded string
+     *
+     * @param password String to be decoded
+     * @return decoded string
+     */
+    public static String decode(String password) {
+        char[] decodedString = new char[password.length()];
+        for (int i = 0; i < password.length(); i++) {
+            decodedString[i] = (char) ((byte)password.charAt(i) ^ encryptionKey);
+        }
+        return new String(decodedString);
+    }
+}
