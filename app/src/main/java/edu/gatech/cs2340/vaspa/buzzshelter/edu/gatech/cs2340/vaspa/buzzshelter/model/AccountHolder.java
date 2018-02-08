3810b7fb54b38530f19b838d0f4638209a15f4b9
snagaraj8 <sanath@gatech.edu>
2018-02-08 01:18:38 -0500

    Separated into packages and added AccountHolder Class

---
+++ app/src/main/java/edu/gatech/cs2340/vaspa/buzzshelter/edu/gatech/cs2340/vaspa/buzzshelter/model/AccountHolder.java
@@ -0,0 +1,12 @@
+package edu.gatech.cs2340.vaspa.buzzshelter.edu.gatech.cs2340.vaspa.buzzshelter.model;
+
+/**
+ * Created by Sanath on 2/8/2018.
+ */
+
+public abstract class AccountHolder {
+    private String userId;
+    private String password;
+    private boolean lockedOut;
+    private String contactInfo;
+}
