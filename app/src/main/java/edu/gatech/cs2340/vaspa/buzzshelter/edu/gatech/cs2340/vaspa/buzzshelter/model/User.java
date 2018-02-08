58e1fb37583780ebfbcd5da6d2e7cce9f97170b0
snagaraj8 <sanath@gatech.edu>
2018-02-08 01:23:12 -0500

    Created User.java

---
+++ app/src/main/java/edu/gatech/cs2340/vaspa/buzzshelter/edu/gatech/cs2340/vaspa/buzzshelter/model/User.java
@@ -0,0 +1,9 @@
+package edu.gatech.cs2340.vaspa.buzzshelter.edu.gatech.cs2340.vaspa.buzzshelter.model;
+
+/**
+ * Created by Sanath on 2/8/2018.
+ */
+
+public class User extends AccountHolder {
+
+}
