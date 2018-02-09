062f27be6bb39edc831599b21f53d916e5fef774
snagaraj8 <sanath@gatech.edu>
2018-02-08 22:38:32 -0500

    Added first iteration of the database (local for now) to use for M4 and M5

--- app/src/main/java/edu/gatech/cs2340/vaspa/buzzshelter/model/AccountHolder.java
+++ app/src/main/java/edu/gatech/cs2340/vaspa/buzzshelter/model/AccountHolder.java
@@ -9,4 +9,25 @@ public abstract class AccountHolder {
     private String password;
     private boolean lockedOut;
     private String contactInfo;
+    public AccountHolder(String userId, String password, boolean lockedOut, String contactInfo) {
+        this.userId = userId;
+        this.password = password;
+        this.lockedOut = lockedOut;
+        this.contactInfo = contactInfo;
+    }
+    public String getUserId() {
+        return userId;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public boolean isLockedOut() {
+        return lockedOut;
+    }
+    public void setLocketOut(boolean isLockedOut) {
+        lockedOut = isLockedOut;
+    }
+    public String getContactInfo() {
+        return contactInfo;
+    }
 }
--- app/src/main/java/edu/gatech/cs2340/vaspa/buzzshelter/model/User.java
+++ app/src/main/java/edu/gatech/cs2340/vaspa/buzzshelter/model/User.java
@@ -10,6 +10,14 @@ public class User extends AccountHolder {
     // TODO: Not sure how to implement MedicalInfo
     private boolean isVeteran;
     // TODO: Not sure how to implement ShelterHistory
-    Shelter currShelter;
-    // TODO: Location
+    public User(String userID, String password, boolean lockedOut, String contactInfo, String gender,
+         int[] dateOfBirth, boolean isVeteran) {
+        super(userID, password, lockedOut, contactInfo);
+        this.gender = gender;
+        this.dateOfBirth = dateOfBirth;
+        this.isVeteran = isVeteran;
+    }
+    public User(String userID, String contactInfo, String password) {
+        this(userID, password, false, contactInfo, "Male", null, false);
+    }
 }
