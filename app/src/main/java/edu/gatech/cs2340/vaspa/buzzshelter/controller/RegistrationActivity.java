ab0e312940d7a0f8bad0350782531f9a677b13a3
AniruddhaDas1998 <aniruddhadas1998@gmail.com>
2018-02-12 20:43:17 -0500

    Added bounds checking and facilitated movement to different registration pages

--- app/src/main/java/edu/gatech/cs2340/vaspa/buzzshelter/controller/RegistrationActivity.java
+++ app/src/main/java/edu/gatech/cs2340/vaspa/buzzshelter/controller/RegistrationActivity.java
@@ -1,8 +1,8 @@
 package edu.gatech.cs2340.vaspa.buzzshelter.controller;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -47,22 +47,62 @@ public class RegistrationActivity extends AppCompatActivity {
         nextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(RegistrationActivity.this,
-                  UserRegistrationActivity.class);
-                startActivity(intent);
-                // nextClicked();
+                 nextClicked();
             }
         });
     }
     void nextClicked() {
-        // checks for passwrd text equality
-        if (!passwordBox.getText().toString().trim().equals(passwordConfirmBox
-          .getText().toString().trim())) {
+        String name = nameBox.getText().toString().trim();
+        String username = usernameBox.getText().toString().trim();
+        String password = passwordBox.getText().toString().trim();
+        String contactInfo = contactInfoBox.getText().toString().trim();
+
+        // checks for password text equality
+
+        if (!password.equals(passwordConfirmBox.getText().toString().trim())) {
             Toast.makeText(RegistrationActivity.this, "Passwords do not match",
-              Toast.LENGTH_SHORT);
+              Toast.LENGTH_SHORT).show();
+
             return;
         }
+
         // implies that passwords are equal
-        // if
+
+        if (name.length() == 0 || username.length() == 0 || password.length() == 0
+          || contactInfo.length() == 0) {
+            Toast.makeText(RegistrationActivity.this, "One or more of the inputted values" +
+              " is empty", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+
+        if (password.length() < 6) {
+            Toast.makeText(RegistrationActivity.this, "Password too short. " +
+                 "Must be atleast 6 characters long", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        String selectedItem = typeReg.getSelectedItem().toString();
+
+        Intent nextIntent;
+
+        if (selectedItem.equalsIgnoreCase("User")) {
+            nextIntent = new Intent(RegistrationActivity.this,
+              UserRegistrationActivity.class);
+        } else if (selectedItem.equalsIgnoreCase("Admin")) {
+            nextIntent = new Intent(RegistrationActivity.this,
+              AdminRegistrationActivity.class);
+        } else {
+            nextIntent = new Intent(RegistrationActivity.this,
+              ShelterEmployeeRegistrationActivity.class);
+        }
+
+        nextIntent.putExtra("name", name);
+        nextIntent.putExtra("username", username);
+        nextIntent.putExtra("password", password);
+        nextIntent.putExtra("contactInfo", contactInfo);
+
+        startActivity(nextIntent);
+
     }
 }
