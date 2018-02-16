416ee6e481f0e803e1735d35c7f518d7938c7f31
snagaraj8 <sanath@gatech.edu>
2018-02-16 14:17:46 -0500

    Added comments for shelter stuff

--- app/src/main/java/edu/gatech/cs2340/vaspa/buzzshelter/model/Shelter.java
+++ app/src/main/java/edu/gatech/cs2340/vaspa/buzzshelter/model/Shelter.java
@@ -1,22 +1,26 @@
 package edu.gatech.cs2340.vaspa.buzzshelter.model;
 
 import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Created by aniruddhadas on 08/02/18.
  */
 
 public class Shelter {
+    // TODO: No args constructor that does nothing
+    // TODO: getters and setters for everthing
+    // TODO: make sure all shits are JSON objects
     private String uniqueKey;
     private String name;
     private int capacity;
     private int vacancies;
     private int latitude;
     private int longitude;
-    private float rating;
-    private HashSet<String> restrictions;
-    private float cost;
+    // private float rating;
+    private Set<String> restrictions;
+    // private float cost;
     private String contactInfo;
-    private User[] blacklist;
-    private User[] currentResidents;
+    // private User[] blacklist;
+    // private User[] currentResidents;
 }
