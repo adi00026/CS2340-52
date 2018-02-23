6758c0540906097b0f078d8378a325bde4c935c4
snagaraj8 <sanath.nagaraj@hotmail.com>
2018-02-23 15:03:14 -0500

    Changed UI and added activities... Made main page baller

---
+++ app/src/main/java/edu/gatech/cs2340/vaspa/buzzshelter/controller/SearchShelterActivity.java
@@ -0,0 +1,15 @@
+package edu.gatech.cs2340.vaspa.buzzshelter.controller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import edu.gatech.cs2340.vaspa.buzzshelter.R;
+
+public class SearchShelterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_shelter);
+    }
+}
