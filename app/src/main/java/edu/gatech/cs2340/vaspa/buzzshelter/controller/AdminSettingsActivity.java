10eab1c641d62692748fb2e6cb9d1be08171d792
snagaraj8 <sanath.nagaraj@hotmail.com>
2018-02-25 19:34:51 -0500

    Added functionality to some auxilliar pages like settings, etc.

--- app/src/main/java/edu/gatech/cs2340/vaspa/buzzshelter/controller/AdminSettingsActivity.java
+++ app/src/main/java/edu/gatech/cs2340/vaspa/buzzshelter/controller/AdminSettingsActivity.java
@@ -1,15 +1,66 @@
 package edu.gatech.cs2340.vaspa.buzzshelter.controller;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import edu.gatech.cs2340.vaspa.buzzshelter.R;
+import edu.gatech.cs2340.vaspa.buzzshelter.model.Admin;
+import edu.gatech.cs2340.vaspa.buzzshelter.model.Model;
 
 public class AdminSettingsActivity extends AppCompatActivity {
+    Button backButton;
+    Button updateButton;
+    Button changePasswordButton;
+    TextView userIDText;
+    EditText nameEditText;
+    EditText contactEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_settings);
+
+        backButton = (Button) findViewById(R.id.button_back);
+        updateButton = (Button) findViewById(R.id.button_update);
+        changePasswordButton = (Button) findViewById(R.id.button_password);
+        userIDText = (TextView) findViewById(R.id.textView_userid);
+        nameEditText = (EditText) findViewById(R.id.editText_name);
+        contactEditText = (EditText) findViewById(R.id.editText_contact);
+
+        userIDText.setText(Model.getInstance().getCurrentUser().getUserId());
+        nameEditText.setText(Model.getInstance().getCurrentUser().getName());
+        contactEditText.setText(Model.getInstance().getCurrentUser().getContactInfo());
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(AdminSettingsActivity.this,
+                        MainPageActivity.class));
+            }
+        });
+
+        changePasswordButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(AdminSettingsActivity.this,
+                        ChangePasswordActivity.class));
+            }
+        });
+
+        updateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                updatePressed();
+            }
+        });
+    }
+
+    private void updatePressed() {
+
     }
 }
--- app/src/main/java/edu/gatech/cs2340/vaspa/buzzshelter/controller/UpdateVacanciesActivity.java
+++ app/src/main/java/edu/gatech/cs2340/vaspa/buzzshelter/controller/UpdateVacanciesActivity.java
@@ -1,15 +1,48 @@
 package edu.gatech.cs2340.vaspa.buzzshelter.controller;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import edu.gatech.cs2340.vaspa.buzzshelter.R;
 
 public class UpdateVacanciesActivity extends AppCompatActivity {
+    Button backButton;
+    Button updateButton;
+    TextView shelterText;
+    EditText vacanciesEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update_vacancies);
+
+        backButton = (Button) findViewById(R.id.button_back);
+        updateButton = (Button) findViewById(R.id.button_update);
+        shelterText = (TextView) findViewById(R.id.textView_shelterName);
+        vacanciesEditText = (EditText) findViewById(R.id.editText_vacancies);
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(UpdateVacanciesActivity.this,
+                        MainPageActivity.class));
+            }
+        });
+
+        updateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                updatePressed();
+            }
+        });
+    }
+
+    private void updatePressed() {
+
     }
 }
