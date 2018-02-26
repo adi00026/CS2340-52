b750726ead716da16febade04c999d28218f5ef6
kauboy26 <kaushik.gc98@gmail.com>
2018-02-26 00:32:18 -0500

    working, but shitty formatting of buttons. Constraints are off. Looks good on Android studio but looks like crap on actual device

--- app/src/main/java/edu/gatech/cs2340/vaspa/buzzshelter/controller/ViewAvailableSheltersActivity.java
+++ app/src/main/java/edu/gatech/cs2340/vaspa/buzzshelter/controller/ViewAvailableSheltersActivity.java
@@ -6,26 +6,39 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import edu.gatech.cs2340.vaspa.buzzshelter.R;
+import edu.gatech.cs2340.vaspa.buzzshelter.model.Model;
+import edu.gatech.cs2340.vaspa.buzzshelter.model.Shelter;
 
 public class ViewAvailableSheltersActivity extends AppCompatActivity {
+
+    TextView shelterInfoTextView;
+    private Model model;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_view_available_shelters);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        setContentView(R.layout.activity_view_shelters);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+        model = Model.getInstance();
+
+        shelterInfoTextView = (TextView) findViewById(R.id.shelterInfoTextView);
+
+        try {
+            if (getIntent().getExtras().containsKey("shelter")) {
+                Shelter sh = (Shelter) getIntent().getExtras().getParcelable("shelter");
+                shelterInfoTextView.setText(sh.getName() + "\n" + sh.getAddress() + "\n" +
+                        "Capacity: " + sh.getCapacity() + "\n"
+                        + "Vacancies: " + sh.getVacancies());
             }
-        });
+        } catch (NullPointerException e) {
+            Toast.makeText(ViewAvailableSheltersActivity.this,
+                    "Sorry, the shelter you requested wasn't found", Toast.LENGTH_LONG).show();
+        }
     }
 
 }
