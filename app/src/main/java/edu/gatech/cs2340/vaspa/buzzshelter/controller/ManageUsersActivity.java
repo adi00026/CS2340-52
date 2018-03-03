5142f23e56fa1139c92d372094ee26c2e67ab09a
snagaraj8 <sanath.nagaraj@hotmail.com>
2018-03-03 15:48:42 -0500

    Working towards adding admin enable/disable functionality

--- app/src/main/java/edu/gatech/cs2340/vaspa/buzzshelter/controller/ManageUsersActivity.java
+++ app/src/main/java/edu/gatech/cs2340/vaspa/buzzshelter/controller/ManageUsersActivity.java
@@ -1,12 +1,25 @@
 package edu.gatech.cs2340.vaspa.buzzshelter.controller;
 
+import android.accounts.Account;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import edu.gatech.cs2340.vaspa.buzzshelter.R;
+import edu.gatech.cs2340.vaspa.buzzshelter.model.AccountHolder;
+import edu.gatech.cs2340.vaspa.buzzshelter.model.Admin;
+import edu.gatech.cs2340.vaspa.buzzshelter.model.Model;
+import edu.gatech.cs2340.vaspa.buzzshelter.model.ShelterEmployee;
+import edu.gatech.cs2340.vaspa.buzzshelter.model.User;
 
 public class ManageUsersActivity extends AppCompatActivity {
     private Button backButton;
@@ -15,10 +28,20 @@ public class ManageUsersActivity extends AppCompatActivity {
     private Button disableButton;
     private Button enableButton;
 
+    FirebaseAuth mAuth;
+    FirebaseDatabase database;
+    DatabaseReference myRef;
+
+    private AccountHolder toManage;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manage_users);
+
+        mAuth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
+        myRef = database.getReference();
 
         backButton = (Button) findViewById(R.id.button_back);
         removeButton = (Button) findViewById(R.id.button_remove);
@@ -72,9 +95,26 @@ public class ManageUsersActivity extends AppCompatActivity {
      *
      * @param status what you want the User's enabled status to be.
      */
-    private void setUserEnabled(boolean status) {
+    private void setUserEnabled(final boolean status) {
         /**
          * Search database. If not found, Toast it. If found, set enabled to false.
          */
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot ds : dataSnapshot.child("account_holders").child("users")
+                        .getChildren()) {
+                    toManage = dataSnapshot.getValue(User.class);
+                    if (toManage != null && toManage.getUserId().equals("Temp")) {
+                        // Disable guy
+                    }
+                    toManage.setLockedOut(status);
+                }
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
 }
