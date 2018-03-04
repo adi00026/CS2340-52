9d6efd054044fe4bebfefa1f1223cda8db311e20
Aniruddha Das <aniruddhadas@Aniruddhas-MacBook-Pro.local>
2018-03-04 15:25:23 -0500

    Added support for adding new shelters

---
+++ app/src/main/java/edu/gatech/cs2340/vaspa/buzzshelter/controller/AdminAddShelterActivity.java
@@ -0,0 +1,15 @@
+package edu.gatech.cs2340.vaspa.buzzshelter.controller;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import edu.gatech.cs2340.vaspa.buzzshelter.R;
+
+public class AdminAddShelterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_add_shelter);
+    }
+}
