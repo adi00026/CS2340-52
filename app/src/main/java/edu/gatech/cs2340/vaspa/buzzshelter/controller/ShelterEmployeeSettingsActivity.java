5d76a76e4e91c9065ad91dd1c35d64abd2c6e80b
AniruddhaDas1998 <aniruddhadas1998@gmail.com>
2018-03-04 02:35:26 -0500

    Added ability to update shelter employees settings

--- app/src/main/java/edu/gatech/cs2340/vaspa/buzzshelter/controller/ShelterEmployeeSettingsActivity.java
+++ app/src/main/java/edu/gatech/cs2340/vaspa/buzzshelter/controller/ShelterEmployeeSettingsActivity.java
@@ -1,12 +1,17 @@
 package edu.gatech.cs2340.vaspa.buzzshelter.controller;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import edu.gatech.cs2340.vaspa.buzzshelter.R;
 import edu.gatech.cs2340.vaspa.buzzshelter.model.Model;
@@ -64,6 +69,22 @@ public class ShelterEmployeeSettingsActivity extends AppCompatActivity {
     }
 
     private void updatePressed() {
+        final ShelterEmployee shelterEmployee = (ShelterEmployee) Model.getInstance().getCurrentUser();
+        String name = nameEditText.getText().toString().trim();
+        String contactInfo = contactEditText.getText().toString().trim();
+        String shelterID = shelterIDEditText.getText().toString().trim();
 
+        if (!name.equals(shelterEmployee.getName()) || !contactInfo.equals(shelterEmployee
+          .getContactInfo()) || !shelterID.equals(shelterEmployee.getShelterID())) {
+            ShelterEmployee newEmployee = new ShelterEmployee(name, shelterEmployee.getUserId(),
+              shelterEmployee.getPassword(), contactInfo, shelterID);
+            String uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
+            final DatabaseReference myRef = FirebaseDatabase.getInstance().getReference();
+            myRef.child("account_holders").child("shelter_employees").child(uid).setValue(newEmployee);
+            Model.getInstance().setCurrentUser(newEmployee);
+        } else {
+            Toast.makeText(ShelterEmployeeSettingsActivity.this,
+              "No attributes are different", Toast.LENGTH_SHORT).show();
+        }
     }
 }
