163b968d50a7d333786db3975c69012f7e75e549
Aniruddha Das <aniruddhadas@Aniruddhas-MacBook-Pro.local>
2018-03-04 00:44:58 -0500

    Added lockout functionality on multiple faulted attempts

--- app/src/main/java/edu/gatech/cs2340/vaspa/buzzshelter/controller/ManageUsersActivity.java
+++ app/src/main/java/edu/gatech/cs2340/vaspa/buzzshelter/controller/ManageUsersActivity.java
@@ -1,8 +1,7 @@
 package edu.gatech.cs2340.vaspa.buzzshelter.controller;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
@@ -76,5 +75,6 @@ public class ManageUsersActivity extends AppCompatActivity {
         /**
          * Search database. If not found, Toast it. If found, set enabled to false.
          */
+
     }
 }
