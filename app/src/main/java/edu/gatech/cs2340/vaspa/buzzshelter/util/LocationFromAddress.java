4003ec0bd6bf610f765fea2166398bb6504b51c8
AniruddhaDas1998 <aniruddhadas1998@gmail.com>
2018-03-21 13:31:32 -0400

    Wrote map method that gets latitude and longitude from google maps

---
+++ app/src/main/java/edu/gatech/cs2340/vaspa/buzzshelter/util/LocationFromAddress.java
@@ -0,0 +1,47 @@
+package edu.gatech.cs2340.vaspa.buzzshelter.util;
+
+import android.content.Context;
+import android.location.Address;
+import android.location.Geocoder;
+
+import com.google.android.gms.maps.model.LatLng;
+
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * Created by aniruddhadas on 21/03/18.
+ */
+
+public class LocationFromAddress {
+    /**
+     * Static method uses Google Maps API to return latitude and longitude based on passed in address
+     *
+     * @param context context to be passed in
+     * @param strAddress address lf location to find latitude and longitude for
+     * @return LatLng object containing Latitude and Longitude of passed in address
+     */
+    public static LatLng getLocationFromAddress(Context context, String strAddress) {
+
+        Geocoder coder = new Geocoder(context);
+        List<Address> address;
+        LatLng latLng = null;
+
+        try {
+            // May throw an IOException
+            address = coder.getFromLocationName(strAddress, 5);
+            if (address == null || address.size() == 0) {
+                // implies backend service is down or no results were found for the address
+                return null;
+            }
+
+            Address location = address.get(0);
+            latLng = new LatLng(location.getLatitude(), location.getLongitude() );
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        // latitude and longitude are public data members of LatLng
+        return latLng;
+    }
+}
