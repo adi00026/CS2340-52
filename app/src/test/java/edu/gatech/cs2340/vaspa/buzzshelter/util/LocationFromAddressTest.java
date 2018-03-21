85bb5c84e6c59fa0b172f37c94785c5326aad75e
Aniruddha Das <aniruddhadas@Aniruddhas-MacBook-Pro.local>
2018-03-21 13:31:32 -0400

    Wrote map method that gets latitude and longitude from google maps

---
+++ app/src/test/java/edu/gatech/cs2340/vaspa/buzzshelter/util/LocationFromAddressTest.java
@@ -0,0 +1,39 @@
+package edu.gatech.cs2340.vaspa.buzzshelter.util;
+
+import android.content.Context;
+import android.test.ServiceTestCase;
+
+import com.google.android.gms.maps.model.LatLng;
+
+import org.junit.Test;
+
+import java.lang.reflect.Method;
+
+/**
+ * Created by aniruddhadas on 21/03/18.
+ */
+
+public class LocationFromAddressTest {
+    public static final int TIMEOUT = 200;
+    @Test
+    public void checkLatLng() {
+        String address = "University House Midtown";
+        LatLng latLng = LocationFromAddress
+          .getLocationFromAddress(getTestContext(), address);
+        System.out.println(latLng.latitude + latLng.longitude);
+    }
+
+    private Context getTestContext()
+    {
+        try
+        {
+            Method getTestContext = ServiceTestCase.class.getMethod("getTestContext");
+            return (Context) getTestContext.invoke(this);
+        }
+        catch (final Exception exception)
+        {
+            exception.printStackTrace();
+            return null;
+        }
+    }
+}
