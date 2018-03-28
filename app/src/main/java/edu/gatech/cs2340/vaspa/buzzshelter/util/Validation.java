9545f1ef00e4362a7c9c55674b51ad96dcfe1654
snagaraj8 <sanath.nagaraj@hotmail.com>
2018-03-28 18:23:50 -0400

    Changed isValidDate method to make easier to write method contract. Changed corresponding JUnit.

--- app/src/main/java/edu/gatech/cs2340/vaspa/buzzshelter/util/Validation.java
+++ app/src/main/java/edu/gatech/cs2340/vaspa/buzzshelter/util/Validation.java
@@ -1,5 +1,7 @@
 package edu.gatech.cs2340.vaspa.buzzshelter.util;
 
+import java.util.List;
+
 /**
  * Class definition for util class Validation.
  *
@@ -10,13 +12,14 @@ public class Validation {
     /**
      * Method to validate an entered date.
      *
-     * @param month month of date to validate
-     * @param day day of date to validate
-     * @param year year of date to validate
+     * @param date list formatted date to validate. Date is in the format {month, day, year}.
      * @return Whether the date is valid
      */
-    public static boolean isValidDate(int month, int day, int year) {
+    public static boolean isValidDate(List<Integer> date) {
         int[] dateArr = {-1, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        int month = date.get(0);
+        int day = date.get(1);
+        int year = date.get(2);
         if (year % 4 == 0) {
             dateArr[2] = 29;
         }
